96f4e1ae8b915e427f405eda4d59e19a83254e71
Rishabh Kushwaha <Rishabhkushwaha9559@gmail.com>
2025-05-10 18:04:56 +0530

    p

---
+++ python/Modulerun.py
@@ -0,0 +1,9 @@
+import calculater as cal
+
+def main():
+    print("factorial :", cal.factoriacl(5))
+    print("square :",cal.square(78))
+    print(f"reverse  string happy :",cal.revesreString("happy") )
+    print(f"reverse  string 121 :",cal.pallindrom("121") )
+    print(f"reverse  string 121 and 456 :",cal.concatString("121","456") )
+main()
---
+++ python/calculater.py
@@ -0,0 +1,30 @@
+# creating module
+
+def square(num):
+  return num*num
+
+
+
+
+def cube(num):
+  return num**3
+
+
+
+def factoriacl(num):
+  if(num==1 or num==0): #base fucntion
+    return 1
+  return num*factoriacl(num-1)
+
+
+def add(num1,num2):
+  return num1+num2
+
+def revesreString(s):
+  return s[: : -1]
+
+def pallindrom(s):
+  return s==s[: : -1]
+
+def concatString(s,t):
+  return s+t
