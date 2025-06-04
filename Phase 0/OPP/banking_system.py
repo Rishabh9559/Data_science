35c13cd8abb78fdb175e73d5dd7622d0861c5304
Rishabh Kushwaha <Rishabhkushwaha9559@gmail.com>
2025-06-04 22:15:02 +0530

    module 0

--- Phase 0/OPP/banking_system.py
+++ Phase 0/OPP/banking_system.py
@@ -3,7 +3,7 @@
 class Account:
    ##  __init__ : constructor to initialize
     def __init__(self,account_number,account_holder,balance):
-        self.acount_number=account_number
+        self.account_number=account_number
         self.account_holder=account_holder
         self.balance=balance
     ## self : used to represent the curretn instant object.
@@ -31,12 +31,48 @@ class Account:
 
 ## Inheritanc : Saving account inherits form account
 
-
+    ## Inheritance ->
 class SavingAccount(Account):
-    def __init__(self, account_number, account_holder, balance,interest_rate,Bank_name):
+    def __init__(self, account_number, account_holder, balance,bank_name,interest_rate):
         super().__init__(account_number, account_holder, balance)
-        self.interst_rate=interest_rate
-        self.Bank_name=Bank_name
+        self.interest_rate=interest_rate
+        self.bank_name=bank_name
     
     ## Polymorphism : 
+    def display(self):
+        print(f"Saving account number :{self.account_number}, balance: {self.balance} ")
     
+    ## Added the interest in the saving account
+    def add_interest(self):
+        interest=self.balance*(self.interest_rate/100)
+        self.balance+=interest
+        print(f"the interest rate {interest}")
+
+
+class CurrentAccount(Account):
+    def __init__(self,account_number,account_holder,balance,overdraft_limit):
+        super().__init__(account_number,account_holder,balance)
+        self.overdraft_limit=overdraft_limit
+
+    ## polymorphism overriding withdraw method
+
+    def withdrow(self, amount):
+        if(amount>=self.balance+self.overdraft_limit):
+            self.balance-=amount
+            print(f"withdraw amount {amount} and current balance {self.balance}")
+        
+        else:
+            print("Overdraf limit over")
+
+
+if __name__=='__main__':
+    acc1=SavingAccount('RK252627','Rishabh kushwaha',99999,'PNB',3)
+    acc1.display()
+    acc1.withdrow(455)
+    acc1.add_interest()
+    acc1.deposit(78545)
+
+
+
+
+
